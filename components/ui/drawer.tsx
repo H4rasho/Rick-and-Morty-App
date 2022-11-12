b71cccc73908d8262784df902ebdbee980408936
ThomasBelrayDev <75146145+H4rasho@users.noreply.github.com>
2022-11-12 16:28:38 +0000

    remove console.log

--- components/ui/drawer.tsx
+++ components/ui/drawer.tsx
@@ -2,8 +2,6 @@ import { Drawer, DrawerContent, DrawerOverlay } from "@chakra-ui/react";
 import Link from "next/link";
 
 export default function CustomDrawer({ isOpen, onClose }: any) {
-  console.log({ isOpen, onClose });
-
   return (
     <Drawer isOpen={isOpen} placement="left" onClose={onClose} size="xs">
       <DrawerOverlay>
