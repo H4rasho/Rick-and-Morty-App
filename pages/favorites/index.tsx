312b55b135280ef57474afe7799c84db0db99607
ThomasBelrayDev <75146145+H4rasho@users.noreply.github.com>
2022-11-14 02:39:34 +0000

    add search favorites page

--- pages/favorites/index.tsx
+++ pages/favorites/index.tsx
@@ -1,22 +1,47 @@
 import { NextPage } from "next";
+import { useRouter } from "next/router";
 import CharacterList from "../../components/characters/character.list";
 import EpisodeList from "../../components/episodes/episode.list";
 import H1 from "../../components/ui/h1";
 import H2 from "../../components/ui/h2";
 import Layout from "../../components/ui/layout";
 import { useLocalStorage } from "../../hooks/useLocalStorage";
+import { Character } from "../../services/characters/types";
+import { Episode } from "../../services/episodes/types";
 
 const Favorites: NextPage = () => {
-  const [characters] = useLocalStorage("FavoriteCharacters", []);
-  const [episodes] = useLocalStorage("FavoriteEpisodes", []);
+  const { query } = useRouter();
+
+  const [characters] = useLocalStorage<Character[]>("FavoriteCharacters", []);
+  const [episodes] = useLocalStorage<Episode[]>("FavoriteEpisodes", []);
+
+  const name = query?.name as string;
+
+  const charactersFiltered = () => {
+    if (name?.length) {
+      return characters.filter((character) =>
+        character.name.toLowerCase().includes(name.toLowerCase())
+      );
+    }
+    return characters;
+  };
+
+  const episodesFiltered = () => {
+    if (name?.length) {
+      return episodes.filter((episode) =>
+        episode.name.toLowerCase().includes(name.toLowerCase())
+      );
+    }
+    return episodes;
+  };
 
   return (
     <Layout title="Favoritos">
       <H1>Favoritos</H1>
       <H2>Personajes</H2>
-      <CharacterList characters={characters} />
+      <CharacterList characters={charactersFiltered()} />
       <H2>Episodios</H2>
-      <EpisodeList episodes={episodes} />
+      <EpisodeList episodes={episodesFiltered()} />
     </Layout>
   );
 };
